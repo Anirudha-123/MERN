3891ab993969043e0543501a67c4d66ae487106c
Anirudha-123 <162452099+Anirudha-123@users.noreply.github.com>
2025-12-07 14:45:53 +0530

    Update CORS configuration to allow multiple origins

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ app.use(express.json())
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin:  [
+    "http://localhost:5173",
+    "https://mern-8tqb.onrender.com"
+  ],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "POST", "PUT", "DELETE"],
